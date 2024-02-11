00fbc0db36f18731b6e7891f2991ce3013e8636d
Bendi116 <142507571+Bendi116@users.noreply.github.com>
2024-02-11 12:28:29 +0100

    Add basic variables

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function subtract(a,b){return a - b}
 function multiple(a,b){return a * b}
 function divide(a,b){return a / b}
 
+//variables
+let firstOperands = null
+let secondOperands = null
+let operator = ""
+
+//
