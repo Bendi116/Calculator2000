d03bb8c00d4ae00e0af22c0dd18402a9e4b49c13
Bendi116 <142507571+Bendi116@users.noreply.github.com>
2024-02-11 12:32:48 +0100

    Add operate function

--- script.js
+++ script.js
@@ -5,8 +5,26 @@ function multiple(a,b){return a * b}
 function divide(a,b){return a / b}
 
 //variables
-let firstOperands = null
-let secondOperands = null
+let firstOperand = null
+let secondOperand = null
 let operator = ""
 
-//
+//operate func
+function operate(firstOperand, secondOperand, operator){
+    let result;
+    switch(operator){
+        case "+":
+            result = add(firstOperand, secondOperand)
+            break
+        case "-":
+            result = subtract(firstOperand, secondOperand)
+            break
+        case "*":
+            result = multiple(firstOperand, secondOperand)
+            break
+        case "/":
+            result = divide(firstOperand, secondOperand)
+            break    
+    }
+    return result;
+}
