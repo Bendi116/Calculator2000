f9b040e5255bb7b59aa94fdeecb8427e74f67e7b
Bendi116 <142507571+Bendi116@users.noreply.github.com>
2024-02-12 10:54:43 +0100

    Add memoryManage, displayand calculate function.

--- script.js
+++ script.js
@@ -5,26 +5,107 @@ function multiple(a,b){return a * b}
 function divide(a,b){return a / b}
 
 //variables
-let firstOperand = null
-let secondOperand = null
+let firstOperand = ""
+let secondOperand = ""
 let operator = ""
+
+let hasOp = false;
+
+///const html element 
+const displayText = document.querySelector(".display-text")
+const numKeys = document.querySelectorAll(".num")
+const opKeys = document.querySelectorAll(".operator-key")
+const equalKey = document.querySelector(".equal-key")
+
+
+
+//EventListeners
+numKeys.forEach((num)=>{
+    num.addEventListener("click",numStoreInMemory)
+})
+opKeys.forEach((op)=>{
+    op.addEventListener("click",operatorStoreInMemory)
+})
+equalKey.addEventListener("click", calculate)
 
 //operate func
 function operate(firstOperand, secondOperand, operator){
     let result;
+    firstOperand = Number(firstOperand);
+    secondOperand = Number(secondOperand);
     switch(operator){
         case "+":
             result = add(firstOperand, secondOperand)
-            break
+            break;
         case "-":
             result = subtract(firstOperand, secondOperand)
-            break
+            break;
         case "*":
             result = multiple(firstOperand, secondOperand)
-            break
+            break;
         case "/":
             result = divide(firstOperand, secondOperand)
-            break    
+            break;
     }
     return result;
 }   
+//calculate function
+function calculate(){
+    let result = operate(firstOperand,secondOperand,operator)
+    displayClear()
+    displayNum(result)
+    setUpMemory()
+}
+
+//memory srote function
+
+function numStoreInMemory(e){
+    manageDisplay()
+    if(operator === ""){
+        firstOperand += e.target.innerText
+        displayNum(e.target.innerText);
+    }else{
+        secondOperand += e.target.innerText
+        displayNum(e.target.innerText)
+    }
+    console.log(firstOperand)
+}
+
+function operatorStoreInMemory(e){
+    operator = e.target.innerText;
+    displayOperator(operator);
+    hasOp = true;
+}
+
+function setUpMemory(){
+    firstOperand = ""
+    secondOperand = ""
+    operator = ""
+}
+//display functions
+function manageDisplay(){
+    if(firstOperand === ""){
+        displayClear()
+    }
+     if(displayText.innerHTML ==="0"){
+        displayClear();
+    }
+
+    if(hasOp){
+        displayClear();
+        hasOp = false;
+    }
+}
+
+function displayClear(){
+    displayText.innerText = "";
+}
+
+function displayOperator(text){
+    displayText.innerText = text;
+}
+
+function displayNum(text){
+    displayText.innerText += text;
+}
+
