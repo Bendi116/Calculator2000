1e3c7120a72c689b4fd5bf096f812cf3262c7a1e
Bendi116 <142507571+Bendi116@users.noreply.github.com>
2024-02-12 12:15:38 +0100

    Add new function to polinom operand in a row, clear, add point etc.

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ let secondOperand = ""
 let operator = ""
 
 let hasOp = false;
+let calculated = false;
+let on = true;
 
 ///const html element 
 const displayText = document.querySelector(".display-text")
 const numKeys = document.querySelectorAll(".num")
 const opKeys = document.querySelectorAll(".operator-key")
 const equalKey = document.querySelector(".equal-key")
+const clearKey = document.querySelector(".clear-key")
+const onOffKey = document.querySelector(".onoff-key")
+const pointKey = document.querySelector(".point-key")
 
 
 
@@ -27,6 +32,9 @@ opKeys.forEach((op)=>{
     op.addEventListener("click",operatorStoreInMemory)
 })
 equalKey.addEventListener("click", calculate)
+clearKey.addEventListener("click",clear)
+onOffKey.addEventListener("click",onOff)
+pointKey.addEventListener("click",addPoint)
 
 //operate func
 function operate(firstOperand, secondOperand, operator){
@@ -49,32 +57,84 @@ function operate(firstOperand, secondOperand, operator){
     }
     return result;
 }   
+//on off function
+function onOff(){
+    on = !on;
+    if(!on){
+        setUpMemory()
+        displayText.innerText = ""
+    }else{
+        displayText.innerText = "0"
+    }
+}
+
+
 //calculate function
+
 function calculate(){
     let result = operate(firstOperand,secondOperand,operator)
     displayClear()
     displayNum(result)
     setUpMemory()
+    firstOperand = result;
+    calculated = true;
+}
+//clear function
+
+function clear(){
+    displayClear()
+    setUpMemory()
+    displayNum("0")
 }
 
 //memory srote function
 
 function numStoreInMemory(e){
-    manageDisplay()
-    if(operator === ""){
-        firstOperand += e.target.innerText
-        displayNum(e.target.innerText);
-    }else{
-        secondOperand += e.target.innerText
-        displayNum(e.target.innerText)
+    if(on){
+        manageDisplay()
+        if(operator === ""){
+            firstOperand += e.target.innerText
+            displayNum(e.target.innerText);
+        }else{
+            secondOperand += e.target.innerText
+            displayNum(e.target.innerText)
+        }
+        calculated = false;
     }
-    console.log(firstOperand)
+    
+    console.log(`firstOperand: ${firstOperand}`)
+    console.log(`secondOperand: ${secondOperand}`)
+
 }
 
 function operatorStoreInMemory(e){
-    operator = e.target.innerText;
-    displayOperator(operator);
-    hasOp = true;
+    if(on){
+        if(operator){
+            calculate()
+        }
+        operator = e.target.innerText;
+        console.log(operator)
+        displayOperator(operator);
+        hasOp = true;
+    }
+   
+
+}
+
+function addPoint(){
+    console.log("Hello")
+    if(secondOperand === "" && firstOperand === "" ){
+        firstOperand+="0."
+        displayNum(".")
+    }
+    else if(secondOperand === "" && firstOperand.indexOf(".") === -1){
+        firstOperand += "."
+        displayNum(".")
+    }else if(secondOperand.indexOf(".") === -1 && secondOperand !==""){
+        console.log(". added")
+        secondOperand += "."
+        displayNum(".")
+    }
 }
 
 function setUpMemory(){
@@ -84,16 +144,17 @@ function setUpMemory(){
 }
 //display functions
 function manageDisplay(){
-    if(firstOperand === ""){
-        displayClear()
-    }
+    console.log(calculated)
      if(displayText.innerHTML ==="0"){
         displayClear();
     }
-
     if(hasOp){
         displayClear();
         hasOp = false;
+    }
+    if(calculated && !operator){
+        firstOperand = "";
+        displayClear();
     }
 }
 
